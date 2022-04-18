bd0d33f33cdd5b1e3b335258cbcc46d8757f6a19
MateusCLMG <mateusmguerra@hotmail.com>
2022-04-18 13:45:23 -0300

    Add files via upload

--- projeto.py
+++ projeto.py
@@ -8,8 +8,9 @@ solutions = [[1,4,6,0,2,7,5,3],[1,4,6,3,0,7,5,2],[1,5,0,6,3,7,2,4],
 [0,4,7,5,2,6,1,3],[0,5,7,2,6,3,1,4],[1,3,5,7,2,0,6,4]]
 
 def flipVertical():
+    solutionsCopy = solutions.copy()
     for i in range(12):
-        x = solutions[i]
+        x = solutionsCopy
         x.reverse()
         solutions.append(x)
 
@@ -39,15 +40,16 @@ for i in range(12):         #filtra solutions por solucoes q começam com o inpu
 
 #//////
 while not linhaAtual == 8:
-    print(tabulate(matchedSol))
+    #print(tabulate(matchedSol))
     x = 100                     # valores quaisquer
     nextCasa = 100              #para serem substituidos
     for i in range(len(matchedSol)):                #buscando nessa lista de possiveis soluções
-        distance = abs(matchedSol[i][linhaAtual] - coluna)   
+        distance = abs(matchedSol[i][linhaAtual] - coluna)
+        print(matchedSol)
         if distance < x:                            #qual solução possui proxima casa mais proxima
             x = distance                            # X eh a menor distancia da rainha anterior para a proxima
             nextCasa = matchedSol[i][linhaAtual]             #next casa diz qual casa sera ocupada
-
+            
     for i in range(len(matchedSol)):                #remove as soluções q nao serao mais usadas 
         try:
             if not (matchedSol[i][linhaAtual] == nextCasa):  
@@ -55,108 +57,29 @@ while not linhaAtual == 8:
         except:
             break
 
-    linhaAtual += 1
+    #linhaAtual += 1
 
     if len(matchedSol) == 1:            # se nao existirem outras soluções, preenche a tabela com o caminho encontrado
         for i in range(8):
             x = matchedSol[0][i]
             tabela[i][x] = 1
         break
+    #print(linhaAtual)
+    tabela[linhaAtual][nextCasa] = 1
+    linhaAtual += 1
+    #print(tabulate(tabela))
 
-    tabela[1][nextCasa] = 1
-    print(tabulate(tabela))
 
 
-
-    print(x)
-    print(nextCasa)
-    print(tabulate(matchedSol))
+    #print(x)
+    #print(nextCasa)
+    #print(tabulate(matchedSol))
 
 print(tabulate(tabela))
     
 
 
 
-#///////////////////////////////////////////////////////////////////////////////
-
-queen = 0   #numero de rainhas no tabuleiro ou linha atual
-
-nextq = 0   #coluna em q a proxima rainha sera posicionada
-
-
-badrows = []    #colunas ja ocupadas por alguma rainha
-
-
-def diagonais(row,column):
-    #cada while preenche as casas nas diagonais
-    x = row
-    y = column
-    k = 0
-    while(k < 8):
-        if(not(tabela[x][y] == 1)):
-            tabela[x][y] = 'X'
-        if(x<7):
-            x += 1
-        if(y<7):
-            y += 1
-        k += 1
-    x = row
-    y = column
-    k = 0
-
-    while(k < 8):
-        if(not(tabela[x][y] == 1)):
-            tabela[x][y] = 'X'
-        if(x>=0):
-            x -= 1
-        if(y<7):
-            y += 1
-        k += 1
-    x = row
-    y = column
-    k = 0
-
-    while(k < 8):
-        if(not(tabela[x][y] == 1)):
-            tabela[x][y] = 'X'
-        if(x<7):
-            x += 1
-        if(y>=0):
-            y -= 1
-        k += 1
-    x = row
-    y = column
-    k = 0
-
-    while(k < 8):
-        if(not(tabela[x][y] == 1)):
-            tabela[x][y] = 'X'
-        if(x>=0):
-            x -= 1
-        if(y>=0):
-            y -= 1
-        k += 1
-    x = row
-    y = column
-    k = 0
-
-
-
-# while(queen <= 7):
-
-#     if(not(badrows.__contains__(nextq)) and tabela[queen][nextq] == 0): #se a coluna nao estiver ocupada
-#         badrows.append(nextq)           #declare como ocupada
-#         tabela[queen][nextq] = 1
-#         diagonais(queen,nextq)
-#         queen += 1
-#     nextq += 1
-
-#     if(nextq == 8):
-#         break
-
-
-
-#print(tabulate(tabela))
 
 
 
